bae08cc41c47c9b2b3a40712cd1fa55f4011dcbb
family-guy <grking8@gmail.com>
2017-03-31 18:16:41 +0000

    Fix bug introduced in 48de219
    Use Mathjax attribute directive to rerender math on homepage via
    ngOnChanges(), MathJax.Hub.Queue().

--- src/app/home.component.spec.ts
+++ src/app/home.component.spec.ts
@@ -1,18 +1,36 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { MathjaxDirective } from '../mathjax/mathjax.directive';
 
-describe('HomeComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        HomeComponent
-      ],
-    }).compileComponents();
-  }));
 
-  it('should create home component', async(() => {
-    const fixture = TestBed.createComponent(HomeComponent);
-    const home = fixture.debugElement.componentInstance;
-    expect(home).toBeTruthy();
-  }));
+describe(
+	'HomeComponent',
+	() => {
+		beforeEach(
+			async(
+				() => {
+					TestBed.configureTestingModule(
+						{
+							declarations: [
+								HomeComponent,
+								MathjaxDirective
+							]
+						}
+					).compileComponents();
+				}
+			)
+		);
+		it(
+			'should create home component',
+			async(
+				() => {
+					const fixture = TestBed.createComponent(HomeComponent);
+					const home = fixture.debugElement.componentInstance;
+					expect(home).toBeTruthy();
+				}
+			)
+		)
+	}
+)
+
--- src/mathjax/mathjax.directive.ts
+++ src/mathjax/mathjax.directive.ts
@@ -1,9 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
+
+declare var MathJax: {
+	Hub: {
+		Queue: (param: Object[]) => void;
+	}
+}
 
 @Directive({
 	selector: '[Mathjax]'
 })
-
 export class MathjaxDirective {
-	
+	  @Input() tex: string;
+	  
+	  constructor(private elt: ElementRef) {}
+	  
+	  ngOnChanges() {
+		  this.elt.nativeElement.innerHTML = this.tex;
+		  MathJax.Hub.Queue(['Typeset', MathJax.Hub, this.elt.nativeElement]);
+	  }
 }

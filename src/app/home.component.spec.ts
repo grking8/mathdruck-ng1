grking8/mathdruck-ng1

commit 48de2196356f8a027c11e220a38a17cb0a93b1aa

Introduce routing
Introduce routing via AppRoutingModule which contains two routes,
'about', 'home'; mapping respectively to new components AboutComponent,
HomeComponent.

Move homepage view out of AppComponent into HomeComponent.

Add navigation bar containing links to new routes to AppComponent.

Add dependencies for AppComponent unit tests.

Introduces bug where math not rendered when navigating between routes.

src/app/home.component.html
@@ -1,0 +1,23 @@
+<div class='container'>
+	
+	<div></div>
+
+	<div>
+		<h1>Share</h1>
+		<h1>your</h1>
+		<h1>math</h1>
+	</div>
+	<div class='right_side'>
+		<ul class='math_symbols'>
+			<li>{{symbols[0]}}</li>
+			<li>{{symbols[1]}}</li>
+			<li>{{symbols[2]}}</li>
+			<li>{{symbols[3]}}</li>
+			<li>{{symbols[4]}}</li>
+			<li>{{symbols[1]}}</li>
+			<li>{{symbols[3]}}</li>
+			<li>{{symbols[3]}}</li>
+			<li>{{symbols[5]}}</li>
+		</ul>
+	</div>
+</div>

src/app/about.component.html
@@ -1,0 +1,3 @@
+<div>
+	<p>{{blurb}}</p>
+</div>

src/app/about.component.ts
@@ -1,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.css']
+})
+export class AboutComponent {  
+	blurb = 'Description of what the app does and how it works.';
+}
+

src/app/app-routing.module.ts
@@ -1,0 +1,31 @@
+// sibling to app.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home.component';
+import { AboutComponent } from './about.component';
+
+const routes: Routes = [
+	{
+		path: 'about',
+		component: AboutComponent
+	},
+	{
+		path: 'home', // just the name of the url 
+		component: HomeComponent
+	},
+	{
+		path: '',
+		redirectTo: '/home',
+		pathMatch: 'full'
+	}
+];
+
+@NgModule(
+	{
+		// no declarations as these are responsibility of companion module
+		imports: [RouterModule.forRoot(routes)],
+		exports: [RouterModule] // allow components in companion module i.e. app.module.ts access to Router declarables e.g. RouterLink and RouterOutlet
+	}
+)
+export class AppRoutingModule {}

src/app/app.component.css
@@ -1,31 +1,3 @@
-li {
-	color: #66ff33;
-	font-size: 3em;
-	list-style-type: none;
-	padding: 30px;
-}
-
-.container {
-	display: -webkit-flex;
-	display: flex;
-	height: 80vh;
-	background-color: #e6e6e6;
-}
-
-.container div {
-	-webkit-flex: 2;
-	flex: 2;
-}
-
-.right_side {
-	background-color: white;
-}
-
-.math_symbols {
-	-moz-column-count: 2;
-	column-count: 2;
-}
-
 footer {
 	height: 7.5vh;
 	background-color: white;

src/app/app.component.html
@@ -1,35 +1,11 @@
-<header>{{title}}</header>
-
-
-
-<div class='container'>
-	
-	<div></div>
-
-	
-	<div>
-		<h1>Share</h1>
-		<h1>your</h1>
-		<h1>math</h1>
-	</div>
-	<div class='right_side'>
-		<ul class='math_symbols'>
-			<li>{{symbols[0]}}</li>
-			<li>{{symbols[1]}}</li>
-			<li>{{symbols[2]}}</li>
-			<li>{{symbols[3]}}</li>
-			<li>{{symbols[4]}}</li>
-			<li>{{symbols[1]}}</li>
-			<li>{{symbols[3]}}</li>
-			<li>{{symbols[3]}}</li>
-			<li>{{symbols[5]}}</li>
-		</ul>
-	</div>
-</div>
-
-
-
-
+<header>
+	{{title}}
+	<nav>
+		<a routerLink='/home' routerLinkActive='active'>HOME</a>
+		<a routerLink='/about' routerLinkActive='active'>ABOUT</a>
+	</nav>
+</header>
+<router-outlet></router-outlet>
 
 <footer>
 	Footer content goes here.

src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +9,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+	  imports: [
+	  	RouterTestingModule
+	  ]
     }).compileComponents();
   }));
 

src/app/app.component.ts
@@ -7,14 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mathDruck';
-  
-  symbols = [
-	  '$\\alpha$',
-	  '$\\mathbb{N}$',
-	  '$\\mathcal{B}$',
-	  '$\\mathbb{R}$',
-	  '$\\beta$',
-	  '$\\vdots$'
-  ];
 }
 

commit bae08cc41c47c9b2b3a40712cd1fa55f4011dcbb

Fix bug introduced in 48de219
Use Mathjax attribute directive to rerender math on homepage via
ngOnChanges(), MathJax.Hub.Queue().

src/app/home.component.html
@@ -9,15 +9,14 @@
 	</div>
 	<div class='right_side'>
 		<ul class='math_symbols'>
-			<li>{{symbols[0]}}</li>
-			<li>{{symbols[1]}}</li>
-			<li>{{symbols[2]}}</li>
-			<li>{{symbols[3]}}</li>
-			<li>{{symbols[4]}}</li>
-			<li>{{symbols[1]}}</li>
-			<li>{{symbols[3]}}</li>
-			<li>{{symbols[3]}}</li>
-			<li>{{symbols[5]}}</li>
+			<li Mathjax tex='{{symbols[0]}}'>{{symbols[0]}}</li>
+			<li Mathjax tex='{{symbols[1]}}'>{{symbols[1]}}</li>
+			<li Mathjax tex='{{symbols[2]}}'>{{symbols[2]}}</li>
+			<li Mathjax tex='{{symbols[3]}}'>{{symbols[3]}}</li>
+			<li Mathjax tex='{{symbols[4]}}'>{{symbols[4]}}</li>
+			<li Mathjax tex='{{symbols[0]}}'>{{symbols[0]}}</li>
+			<li Mathjax tex='{{symbols[1]}}'>{{symbols[1]}}</li>
+			<li Mathjax tex='{{symbols[5]}}'>{{symbols[5]}}</li>
 		</ul>
 	</div>
 </div>

src/mathjax/mathjax.directive.ts
@@ -1,9 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
+
+declare var MathJax: {
+	Hub: {
+		Queue: (param: Object[]) => void;
+	}
+}
 
 @Directive({
 	selector: '[Mathjax]'
 })
-
 export class MathjaxDirective {
-	
+	  @Input() tex: string;
+	  
+	  constructor(private elt: ElementRef) {}
+	  
+	  ngOnChanges() {
+		  this.elt.nativeElement.innerHTML = this.tex;
+		  MathJax.Hub.Queue(['Typeset', MathJax.Hub, this.elt.nativeElement]);
+	  }
 }

src/app/home.component.spec.ts
@@ -1,18 +1,36 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { MathjaxDirective } from '../mathjax/mathjax.directive';
 
-describe('HomeComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        HomeComponent
-      ],
-    }).compileComponents();
-  }));
 
-  it('should create home component', async(() => {
-    const fixture = TestBed.createComponent(HomeComponent);
-    const home = fixture.debugElement.componentInstance;
-    expect(home).toBeTruthy();
-  }));
+describe(
+	'HomeComponent',
+	() => {
+		beforeEach(
+			async(
+				() => {
+					TestBed.configureTestingModule(
+						{
+							declarations: [
+								HomeComponent,
+								MathjaxDirective
+							]
+						}
+					).compileComponents();
+				}
+			)
+		);
+		it(
+			'should create home component',
+			async(
+				() => {
+					const fixture = TestBed.createComponent(HomeComponent);
+					const home = fixture.debugElement.componentInstance;
+					expect(home).toBeTruthy();
+				}
+			)
+		)
+	}
+)
+