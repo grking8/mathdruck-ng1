0a286ce8307bece3ff48082c29a1ac695e00c2dd
family-guy <grking8@gmail.com>
2017-04-03 11:20:49 +0000

    Name changes

--- src/text-editor/text-editor.component.ts
+++ src/text-editor/text-editor.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 export class TextEditorComponent {
 	userTex: string;
 	
-	placeholder = `Soient $n$ dans $\\mathbb{N}^*$,
+	placeholderTex = `Soient $n$ dans $\\mathbb{N}^*$,
 $a_1,\\ldots,a_n,b_1,\\ldots,b_n$ des réels. Montrer que :
 $$\\left\\lvert\\sum_{i=1}^{n}a_{i}b_{i}\\right\\rvert
 \\leq\\sqrt{\\sum_{i=1}^{n}a_i^2}\\sqrt{\\sum_{i=1}^{n}b_i^2}$$`;
