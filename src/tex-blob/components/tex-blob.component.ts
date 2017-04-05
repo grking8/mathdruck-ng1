5943dbfa13ffdf61d81c8f4ef352ce751a9afa45
family-guy <grking8@gmail.com>
2017-04-05 10:38:15 +0000

    Create injection site in texblob component
    Make texblob component aware of texblob service.
    
    Make texblob component inject instance of texblob service upon
    instantiation.
    
    Wrap `getTexBlob` call (method of texblob service) in dummy method.

--- src/tex-blob/components/tex-blob.component.ts
+++ src/tex-blob/components/tex-blob.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 
+import { TexBlob } from '../tex-blob';
+import { TexBlobService } from '../services/tex-blob.service';
+
 @Component({
 	selector: 'tex-blob',
 	templateUrl: './tex-blob.component.html',
-	styleUrls: ['./tex-blob.component.css']
+	styleUrls: ['./tex-blob.component.css'],
+	providers: [TexBlobService] // tell injector how to make a TexBlobService
 })
 
 export class TexBlobComponent {
+	texBlob: TexBlob;
 	
+	constructor(private texBlobService: TexBlobService) {} // does nothing except define private property and identify it as a TexBlobService injection site. Thus Angular knows to supply an instance of the TexBlobService when it creates a TexBlobComponent
+	
+	getTexBlob(): void {
+		this.texBlob = this.texBlobService.getTexBlob(0);
+	}
 }
