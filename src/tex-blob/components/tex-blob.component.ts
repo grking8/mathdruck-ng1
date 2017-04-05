f9a877b317fc7f5125ba10f73ae2acf4b43a4156
family-guy <grking8@gmail.com>
2017-04-05 11:02:23 +0000

    Make texblob component implement OnInit interface
    Address question of when/how to call texblob component's `getTexBlob`
    method.

--- src/tex-blob/components/tex-blob.component.ts
+++ src/tex-blob/components/tex-blob.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 import { TexBlob } from '../tex-blob';
 import { TexBlobService } from '../services/tex-blob.service';
@@ -10,12 +11,16 @@ import { TexBlobService } from '../services/tex-blob.service';
 	providers: [TexBlobService] // tell injector how to make a TexBlobService
 })
 
-export class TexBlobComponent {
+export class TexBlobComponent implements OnInit {
 	texBlob: TexBlob;
 	
 	constructor(private texBlobService: TexBlobService) {} // does nothing except define private property and identify it as a TexBlobService injection site. Thus Angular knows to supply an instance of the TexBlobService when it creates a TexBlobComponent
 	
 	getTexBlob(): void {
 		this.texBlob = this.texBlobService.getTexBlob(0);
+	}
+	
+	ngOnInit(): void {
+		this.getTexBlob();
 	}
 }
