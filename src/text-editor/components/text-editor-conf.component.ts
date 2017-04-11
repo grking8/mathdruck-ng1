759b780b89a90fc3972870dcde1bc85e0f60dae7
family-guy <grking8@gmail.com>
2017-04-11 16:27:03 +0000

    Styling and content

--- src/text-editor/components/text-editor-conf.component.ts
+++ src/text-editor/components/text-editor-conf.component.ts
@@ -9,5 +9,24 @@ export class TextEditorConfComponent {
 	msg = `Thanks for sharing. Your math is available at
 	http://localhost:4200/shared/1`;
 	
-	tex = `Add some example, static math here e.g. $x$`;
+	tex = `$\\begin{align*}
+	\\left(a_ix+b_i\\right)^2&=
+	a_i^2x^2+b_i^2+2a_ib_ix\\\\
+	\\Rightarrow\\sum_{i=1}^n\\left(a_ix+b_i\\right)^2
+	&=\\sum_{i=1}^n\\left(a_i^2x^2+b_i^2+2a_ib_ix\\right)
+	\\end{align*}$
+	<br><br>
+	Or,
+	<br><br>
+	$\\begin{align*}
+	\\sum_{i=1}^n\\left(a_i^2x^2+b_i^2+2a_ib_ix\\right)
+	&=\\left(\\sum_{i=1}^na_i^2\\right)x^2
+	+\\left(2\\sum_{i=1}^na_ib_i\\right)x\\\\
+	&+\\sum_{i=1}^nb_i^2
+	\\end{align*}$
+	<br><br>
+	D'où
+	<br><br>
+	$$\\left(2\\sum_{i=1}^na_ib_i\\right)^2
+	-4\\sum_{i=1}^na_i^2\\sum_{i=1}^nb_i^2\\leq0$$`;
 }
