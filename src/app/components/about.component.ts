2bb01a2f036ce008d71e79811d4792ca8afad8dd
family-guy <grking8@gmail.com>
2017-05-04 11:47:27 +0000

    About page content

--- src/app/components/about.component.ts
+++ src/app/components/about.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
 	styleUrls: ['./about.component.css']
 })
 export class AboutComponent {  
-	blurb = 'Description of what the app does and how it works.';
+	blurb = [
+		'Desktop LaTeX editors, e.g. TeXmaker, are ideal for writing scientific papers and publishing. However, when writing smaller texts, users can sometimes find such editors unwieldy. Moreover, sharing PDFs frequently can be time-consuming.',
+		'Mathdruck allows users to write LaTeX directly into their browser and see the results as they type. Once done, they can share their work with others by sending them a URL.',
+		'The app is aimed at students, researchers, and teachers who need to write math. For example, some students decide they wish to work on a homework assignment together and set up a group chat. They usually upload to the chat images containing their math, whether handwritten or typed.',
+		'With Mathdruck, they can instead simply type their math in one tab, and copy and paste a URL into another tab. The others in the group can then see that person\'s work by simply clicking on the URL.'
+	]
 }
 
